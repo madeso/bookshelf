d52db8e3d0432bbd8fffedb2c94d0ccbb7671093
Gustav <sir.gustav.the.coder@gmail.com>
2024-10-04 21:13:18 +0200

    Add 97 things sample book

---
+++ 97things.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import glob
+from typing import List
+
+
+def fix(dir: str):
+    print(dir)
+
+    entries = (os.path.join(dir, p) for p in os.listdir(dir))
+    dirs = (p for p in entries if os.path.isdir(p))
+    for d in dirs:
+        src = os.path.join(d, 'readme.md')
+        lines = []
+        with open(src, 'r') as f:
+            lines = f.readlines()
+        if len(lines) == 0:
+            print(f"Empty file: {src}")
+            continue
+        if not lines[0].startswith('# '):
+            print(f"Not a valid file: {src}")
+            continue
+        title = lines[0][2:].strip()
+        name = os.path.split(d)[1]
+        weight = name.split('_')[1].lstrip('0')
+        tgt = os.path.join(dir, f"{name}.md")
+        escaped_title = title.replace('"', '\\"')
+        with open(tgt, 'w') as f:
+            f.write('+++\n')
+            f.write(f"title = \"{escaped_title}\"\n")
+            f.write(f"weight = {weight}\n")
+            f.write('+++\n')
+            f.write(''.join(lines[1:]))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", help="The name of the file or directories to glob to split", nargs='+')
+    args = parser.parse_args()
+
+    for filename in args.filenames:
+        fix(os.path.abspath(filename))
+
+if __name__ == "__main__":
+    main()
---
+++ content/books/97things/_index.md
@@ -0,0 +1,14 @@
++++
+title = '97 Things Every Programmer Should Know'
+tags = ['test', 'tech']
+no_prev = true
+no_next = false
++++
+
+_Pearls of wisdom for programmers collected from leading practitioners._
+
+This is a [GitBook](https://www.gitbook.io) version of the ['97 Things Every Programmer Should Know' project](http://programmer.97things.oreilly.com/wiki/index.php/97_Things_Every_Programmer_Should_Know).
+
+All content is licensed under the [Creative Commons Attribution Non Commercial Share Alike 3.0 license](http://creativecommons.org/licenses/by-nc-sa/3.0/). Print versions of the book are available on [Amazon.com](http://www.amazon.com/Things-Every-Programmer-Should-Know/dp/0596809484).
+
+All content copied from [github](https://github.com/97-things/97-things-every-programmer-should-know/)
